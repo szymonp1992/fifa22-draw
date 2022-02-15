2cd0c53abb6ce9c6a8f921379e5f3199ee1af200
szymonp1992 <s.podstufka@gmail.com>
2022-02-15 20:03:15 +0100

    Dropdown menu color change after any option selected (excl. default 'Any'

--- dropdown-menu.js
+++ dropdown-menu.js
@@ -19,15 +19,17 @@ data.forEach(r => uniqueOptions1.add(r[0]))
 
 const uniqueList1 = [...uniqueOptions1]
 
-const populateLevel1 = function () {
+const populateLevel1 = () => {
+    selectLevel1.innerHTML = `<option value="">Any</option>`
     for (let i = 0; i < uniqueList1.length; i++) {
         const option = `<option value="${uniqueList1[i].toLowerCase().replace(" ", "-")}">${uniqueList1[i]}</option>`;
         selectLevel1.innerHTML += option;
     }
 }
 
-const populateLevel2 = function () {
-    selectLevel2.innerHTML = ""
+const populateLevel2 = () => {
+    selectLevel2.innerHTML = `<option value="">Any</option>`
+    selectLevel2.style.backgroundColor = "var(--element-color)";
     const level1 = selectLevel1.value;
     const filteredArray = data.filter(r => r[0].toLowerCase() === level1)
     const uniqueOptions2 = new Set();
@@ -39,7 +41,21 @@ const populateLevel2 = function () {
     }
 }
 
+const changeSelectColor = (e) => {
+    if (e.target.value) {
+        e.target.style.backgroundColor = "var(--hover-color)"
+    }
+    if (e.target.value === "") {
+        e.target.style.backgroundColor = "var(--element-color)"
+    }
+    if (e.target.value === selectLevel2.value && e.target.value === "") {
+        selectLevel2.style.backgroundColor = "var(--element-color)"
+    }
+}
+
 populateLevel1();
 populateLevel2();
 
-selectLevel1.addEventListener("change", populateLevel2)
+selectLevel1.addEventListener("change", populateLevel2);
+selectLevel1.addEventListener("change", changeSelectColor);
+selectLevel2.addEventListener("change", changeSelectColor);
